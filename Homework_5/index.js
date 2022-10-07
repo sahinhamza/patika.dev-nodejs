c2bebaa801bf4caecd8ae84221d82e09b04db72b
hamzasahin <hmzshn_11@hotmail.com>
2022-10-07 20:12:44 +0300

    homework5 done

---
+++ Homework_5/index.js
@@ -0,0 +1,26 @@
+const https = require("http");
+
+const server = https.createServer((req, res) => {
+    const url = req.url;
+
+    if (url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h2>INDEX SAYFASINA HOSGELDINIZ</h2>");
+      } else if (url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h2>ABOUT SAYFASINA HOSGELDINIZ</h2>");
+      } else if (url === "/contact") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h2>CONTACT SAYFASINA HOSGELDINIZ</h2>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write("<h2>404 SAYFA BULUNAMADI</h2>");
+      }
+    
+      res.end();
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Sunucu port ${port} de başlatıldı.`);
+});
