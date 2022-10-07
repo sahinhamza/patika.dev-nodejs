78d24c8510b555b0678f9e6f9ac128788ca55b9e
hamzasahin <hmzshn_11@hotmail.com>
2022-10-08 00:47:57 +0300

    homework8 done

--- CleanBlog/app.js
+++ CleanBlog/app.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
 
 const app = new express();
 
-app.get("/", (req,res)=>{
-    blog = { id: 1, title: "Blog title", description: "Blog description" };
-    res.send(blog);
+//TEMPLATE ENGINE
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs")
+
+//MIDDLEWARES
+app.use(express.static('CleanBlog/public'));
+
+//ROUTES
+app.get("/", (req, res) => {
+    res.render("index")
+});
+
+app.get("/about", (req, res) => {
+    res.render("about")
+});
+
+app.get("/add_post", (req, res) => {
+    res.render("add_post")
 });
 
 const port = 3000;
